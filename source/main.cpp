cb3e3ab74346ac74627e06e0126854631addd130
Maxime Boucher <mxboucher@gmail.com>
2021-04-04 18:25:11 +0200

    lumiere speculaire bouge

--- source/main.cpp
+++ source/main.cpp
@@ -250,7 +250,6 @@ vec4 castRay(vec4 p0, vec4 E, Object *lastHitObject, int depth){
 
     vec4 N = normalize(intersections[closestObject].N);
     vec4 L = normalize(lightPosition - intersections[closestObject].P); // lumière
-    vec4 C = normalize(cameraPosition - intersections[closestObject].P); // caméra
     vec4 reflection = normalize(reflect(L, N));
 
     color4 material_ambient(
@@ -275,7 +274,7 @@ vec4 castRay(vec4 p0, vec4 E, Object *lastHitObject, int depth){
 
     color4 ambient_product = GLState::light_ambient * material_ambient;
     color4 diffuse_product = GLState::light_diffuse * material_diffuse * max(0.0f, dot(L, N));
-    color4 specular_product = GLState::light_specular * material_specular * pow(max(0.0f, dot(C, reflection)), material_shininess);
+    color4 specular_product = GLState::light_specular * material_specular * pow(max(0.0f, dot(reflection, E)), material_shininess);
 
     color *= (ambient_product + diffuse_product + specular_product);
     
